fcb20f604d100989979e318be1238aa7ba04ae33
zaibe_.x <136946163+shahzaib512@users.noreply.github.com>
2023-09-18 10:50:52 +0500

    forms and useraccounts

--- blog/settings.py
+++ blog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogs.apps.BlogsConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
+
+
--- blogs/tests.py
+++ blogs/tests.py
@@ -1,13 +1,14 @@
 from django.test import TestCase
 from django.urls import reverse
 from .models import Post
-from django.contrib.auth.models import User
+from django.contrib.auth.models import get_user_model
 
 class BlogTests(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create_user(
+        self.user = get_user_model().objects.create_user(
             username='testuser',
+            email='test@email.com',
             password='testpassword'
         )
         self.post = Post.objects.create(
@@ -15,26 +16,44 @@ class BlogTests(TestCase):
             author=self.user,
             body='Test Body Content'
         )
+    def test_string_representation(self):
+        post = Post(title='A sample title')
+        self.assertEqual(str(post), post.title)
+    
 
-    def test_blog_content(self):
-        post = Post.objects.get(id=1)
-        expected_author = f'{self.user}'
-        expected_title = 'Test Title'
-        expected_body = 'Test Body Content'
-        self.assertEqual(expected_author, str(post.author))
-        self.assertEqual(expected_title, post.title)
-        self.assertEqual(expected_body, post.body)
-
-    def test_blog_list_view(self):
+    
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/')
+    def test_post_content(self):
+        self.assertEqual(f'{self.post.title}', 'A good title')
+        self.assertEqual(f'{self.post.author}', 'testuser')
+        self.assertEqual(f'{self.post.body}', 'Nice body content')
+    def test_post_list_view(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Test Title')
+        self.assertContains(response, 'Nice body content')
         self.assertTemplateUsed(response, 'home.html')
-
-    def test_blog_detail_view(self):
+    def test_post_detail_view(self):
         response = self.client.get('/post/1/')
         no_response = self.client.get('/post/100000/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
-        self.assertContains(response, 'Test Title')
+        self.assertContains(response, 'A good title')
         self.assertTemplateUsed(response, 'post_detail.html')
+    def test_post_create_view(self): # new
+        response = self.client.post(reverse('post_new'), {
+            'title': 'New title',
+            'body': 'New text',
+            'author': self.user,})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'New title')
+        self.assertContains(response, 'New text')
+    def test_post_update_view(self): # new
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Updated title',
+            'body': 'Updated text',})
+        self.assertEqual(response.status_code, 302)
+    def test_post_delete_view(self): # new
+        response = self.client.post(
+            reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 302)
